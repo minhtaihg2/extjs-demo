ade33d951ad90535f8e347bf31f2a15551597894
Harvey Pham <harvey.pham@qsoftvietnam.com>
2016-03-16 10:35:34 +0700

    Add banner login

--- app/AppConfig.js
+++ app/AppConfig.js
@@ -1,7 +1,7 @@
 Ext.define('MyApp.AppConfig',{
    singleton : true,
     api : {
-        ApiUrlLogin : 'http://192.168.1.87/ypc571/public/v1/auth/login',
+        ApiUrlLogin : 'http://52.29.108.195/dev/api/v1/auth/login',
         ApiClient : 'http://192.168.1.87/ypc571/public/v1/administrator/users'
     }
 });
